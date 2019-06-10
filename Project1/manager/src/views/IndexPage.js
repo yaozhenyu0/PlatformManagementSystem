48a9a1c1e300e40a81c9718d51f7190c9b2f47d2
ycn <ycn@169.com>
2019-06-10 21:42:10 +0800

    登录

--- Project1/manager/src/views/IndexPage.js
+++ Project1/manager/src/views/IndexPage.js
@@ -1,21 +1,68 @@
-import React from 'react';
+
 import { connect } from 'dva';
 import styles from './IndexPage.css';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import 'antd/dist/antd.css';
+import { createPublicKey } from 'crypto';
+import React,{Component} from 'react';
+import { Script } from 'vm';
 
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
-    </div>
-  );
+class IndexPage extends Component{
+  render(){
+    return (
+      <div className={styles.root}>
+        <div className={styles.box}>
+            <Form className="login-form">
+            <div className={styles.username}>
+                <Form.Item>
+                  {
+                    <Input
+                      prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      placeholder="Username"
+                    />
+                  }
+                </Form.Item>
+                <Form.Item>
+              {
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="password"
+                  placeholder="Password"
+                />
+              }
+            </Form.Item>
+            </div>
+            
+            <Form.Item>
+              <div className={styles.left}>
+                 {<Checkbox>记住密码</Checkbox>}
+              </div>
+              <div className={styles.right}>
+                  <a className="login-form-forgot" href="">
+                    忘记密码
+                  </a>
+              </div>
+                  <Button type="primary" htmlType="submit" className={styles.btn} onClick={this.handleSubmit.bind(this)}>
+                    登陆
+                  </Button>
+             
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+      );
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    })
+  }
 }
-
 IndexPage.propTypes = {
+  
 };
 
 export default connect()(IndexPage);
