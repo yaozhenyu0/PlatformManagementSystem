41335b08320bc51fdd7b386889206790e1fbb9c7
ycn <ycn@169.com>
2019-06-11 12:01:09 +0800

    dome

--- Project1/manager/src/views/domePage.js
+++ Project1/manager/src/views/domePage.js
@@ -24,10 +24,12 @@ class domePage extends Component {
   };
 
   render() {
-    return (
+    return (<div>
+        <div className={styles.header}>
+              {/* <Header>Header</Header> */}
+        </div>
       <Layout className={styles.box}>
-        <Header>Header</Header>
-      <Sider>
+      <Sider className={styles.slider}>
       <Menu
         mode="inline"
         openKeys={this.state.openKeys}
@@ -40,44 +42,64 @@ class domePage extends Component {
           title={
             <span>
               <Icon type="mail" />
-              <span>Navigation One</span>
+              <span>试题管理</span>
             </span>
           }
         >
-          <Menu.Item key="1">Option 1</Menu.Item>
-          <Menu.Item key="2">Option 2</Menu.Item>
-          <Menu.Item key="3">Option 3</Menu.Item>
-          <Menu.Item key="4">Option 4</Menu.Item>
+          <Menu.Item key="1">添加试题</Menu.Item>
+          <Menu.Item key="2">试题分类</Menu.Item>
+          <Menu.Item key="3">查看试题</Menu.Item>
         </SubMenu>
         <SubMenu
           key="sub2"
           title={
             <span>
               <Icon type="appstore" />
-              <span>Navigation Two</span>
+              <span>用户管理</span>
             </span>
           }
         >
-          <Menu.Item key="5">Option 5</Menu.Item>
-          <Menu.Item key="6">Option 6</Menu.Item>
-          <SubMenu key="sub3" title="Submenu">
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
-          </SubMenu>
+          <Menu.Item key="4">添加用户</Menu.Item>
+          <Menu.Item key="5">用户展示</Menu.Item>
         </SubMenu>
         <SubMenu
           key="sub4"
           title={
             <span>
               <Icon type="setting" />
-              <span>Navigation Three</span>
+              <span>考试管理</span>
             </span>
           }
         >
-          <Menu.Item key="9">Option 9</Menu.Item>
-          <Menu.Item key="10">Option 10</Menu.Item>
-          <Menu.Item key="11">Option 11</Menu.Item>
-          <Menu.Item key="12">Option 12</Menu.Item>
+          <Menu.Item key="6">添加考试</Menu.Item>
+          <Menu.Item key="7">试卷列表</Menu.Item>
+        </SubMenu>
+
+        <SubMenu
+          key="sub2"
+          title={
+            <span>
+              <Icon type="appstore" />
+              <span>班级管理</span>
+            </span>
+          }
+        >
+          <Menu.Item key="8">班级管理</Menu.Item>
+          <Menu.Item key="9">教室管理</Menu.Item>
+          <Menu.Item key="10">学生管理</Menu.Item>
+        </SubMenu>
+       
+      
+        <SubMenu
+          key="sub4"
+          title={
+            <span>
+              <Icon type="setting" />
+              <span>阅卷管理</span>
+            </span>
+          }
+        >
+          <Menu.Item key="11">待批班级</Menu.Item>
         </SubMenu>
       </Menu>
       </Sider>
@@ -85,7 +107,7 @@ class domePage extends Component {
         <Content>Content</Content>
      
     </Layout>
-     
+    </div>
     );
   }
 }
